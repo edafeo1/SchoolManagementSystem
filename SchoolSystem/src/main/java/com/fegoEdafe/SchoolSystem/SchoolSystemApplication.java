6761c19dd22d2137003bf0d7f0fcff6cd6a90dbd
Ofego Edafe <rawcode@Ofegos-MBP.modem>
2020-12-18 21:30:54 +1100

    c

--- SchoolSystem/src/main/java/com/fegoEdafe/SchoolSystem/SchoolSystemApplication.java
+++ SchoolSystem/src/main/java/com/fegoEdafe/SchoolSystem/SchoolSystemApplication.java
@@ -2,7 +2,7 @@ package com.fegoEdafe.SchoolSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SchoolSystemApplication {
 
